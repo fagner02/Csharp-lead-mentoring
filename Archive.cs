328343355dd735462fcdf6f7301a039c96c80b18
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2021-11-06 20:55:33 -0300

    first run

---
+++ Archive.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using MovieClass;
+
+namespace ArchiveClass {
+    class Archive {
+        public List<Movie> movies = new List<Movie>();
+        public List<Movie> deactiveMovies = new List<Movie>();
+        
+        public Archive(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        public void addMovie() {
+            Movie temp = new Movie();
+            Console.WriteLine("Enter the name of the movie: ");
+            temp.title = Console.ReadLine();
+            Console.WriteLine("Enter the genre of the movie: ");
+            temp.genre = Console.ReadLine();
+            Console.WriteLine("Enter the rating of the movie: ");
+            temp.rating = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the movie's date: year, month, day ");
+            int year = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            temp.releaseDate = new DateTime(year, month, day);
+            Console.WriteLine("Enter the name of the director: ");
+            temp.director = Console.ReadLine();
+            string input = "";
+            Console.WriteLine("Enter the actors name and when you're done enter \'done\'");
+            while(input != "done") {
+                //Console.WriteLine("Enter actor name: ");
+                input = Console.ReadLine();
+                if(input != "done") {
+                    temp.actors.Add(input);
+                }
+            }
+            Console.WriteLine("Enter the synopsis of the movie: ");
+            temp.plot = Console.ReadLine();
+            Console.WriteLine("Enter the movie's date: year, month, day ");
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            temp.duration = new TimeSpan(hour, min, 0);
+
+            if(movies.Exists(x => x.title == temp.title)){
+                return;
+            }
+            movies.Add(temp);
+        }
+
+        public void deactivateMovie() {
+            string title = Console.ReadLine();
+            Movie movie = movies[movies.FindIndex(x => x.title == title)];
+            deactiveMovies.Add(movie);
+            movies.Remove(movie);
+        }
+
+        public void printMovies() {
+            foreach (Movie movie in movies) {
+                movie.print();
+            }
+        }
+
+        public void printDeactiveMovies() {
+            foreach (Movie movie in deactiveMovies) {
+                movie.print();
+            }
+        }
+
+        public void printMovie(){
+            Console.WriteLine("Enter the movie title: ");
+            string title = Console.ReadLine();
+            if(!movies.Exists(x => x.title == title)) {
+                Console.WriteLine("Movie not found");
+                return;
+            }
+            movies.Find(x => x.title == title).print();
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using MovieClass;
+using ArchiveClass;
 
-namespace filmes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace filmes {
+    class Program {
+
+        static List<Movie> movies = new List<Movie>() {
+            new Movie("The Shawshank Redemption", "Adventure", 10, new DateTime(2000, 4, 5), "Frank Darabont", new List<string>{"Nicole Kidman"}, "Crime, Drama", new TimeSpan(2, 56, 0)),
+            new Movie("Bulsula de ouro", "Adventure", 10, new DateTime(2000, 4, 5), "Frank Darabont", new List<string>{"Nicole Kidman"}, "Crime, Drama", new TimeSpan(2, 40, 0))
+        };
+
+        static void Main(string[] args) {
+            Archive archive = new Archive(movies);
+            string[] commands = {
+                "add",
+                "remove",
+                "list-active",
+                "list-deactive",
+                "print-movie",
+                "exit"
+            };
+
+            string cmd = "";
+            while(cmd != "exit"){
+                Console.WriteLine("Enter command");
+                cmd = Console.ReadLine();
+                if(cmd == "add") {
+                    archive.addMovie();
+                }
+                if(cmd == "remove") {
+                    archive.deactivateMovie();
+                }
+                if(cmd == "list-active") {
+                    archive.printMovies();
+                }
+            }
+
+            return;
         }
     }
 }
