126b5c63239cad64eb00ea00fe3d0ab12007e961
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2021-11-15 12:41:37 -0300

    Add description to subject

--- Subject.cs
+++ Subject.cs
@@ -3,9 +3,7 @@ namespace SchoolLib
     public class Subject
     {
         public string name { get; set; }
-        public string ClassTeacher { get; set; }
-        public int ClassNumber { get; set; }
-        public int ClassCapacity { get; set; }
+        public string description;
 
         public Subject(string name)
         {
